26e529b6b4d0b7d21ffe48d8d5aa0c19ff64d64a
Mihajlo Jovanovic <mihajlo.coder@gmail.com>
2023-01-09 23:23:12 -0500

    cleaned up solution for day 24. runs in about 40 sec and 2 mins for parts 1 & 2, but is ~100 LoC

--- 2022/clojure/day24/src/day24/core.clj
+++ 2022/clojure/day24/src/day24/core.clj
@@ -1,10 +1,12 @@
 (ns day24.core
   (:require
+    [clojure.java.io :as io]
     [clojure.set :as set]
     [clojure.string :as str])
   (:gen-class))
 
 (defn parse-line [[x s]]
+  "Helper function for parse-blizzards - parse a single line starting with index"
   (let [l (->> s
                drop-last
                (drop 1)
@@ -14,17 +16,21 @@
         xf (into [] xform l)]
     (map #(assoc % :x x) xf)))
 
-(defn parse-grid [s]
-  (->> s
-       str/split-lines
-       drop-last
-       (drop 1)
-       (map-indexed vector)
-       (map parse-line)
-       (filter not-empty)
-       flatten))
-
-;(def xform (map (fn [n] {:y (first n) :d (last n)})))
+(defn parse-blizzards [s]
+  "Returns a map with keys containing blizzard coordinares, as well max x & y values"
+  (let [lines (->> s
+                   str/split-lines
+                   drop-last
+                   (drop 1))
+        max-x (count lines)
+        max-y (- (count (first lines)) 2)]
+    {:grid  (->> lines
+                 (map-indexed vector)
+                 (map parse-line)
+                 (filter not-empty)
+                 flatten)
+     :max-x max-x
+     :max-y max-y}))
 
 (defn move [b max-x max-y]
   (case (:d b)
@@ -32,57 +38,64 @@
     \< (update b :y #(mod (dec %) max-y))
     \v (update b :x #(mod (inc %) max-x))
     \^ (update b :x #(mod (dec %) max-x))))
+(def start-pos {:x -1 :y 0})
 
-(def init-state {:e {:x -1 :y 0} :grid (parse-grid "#.########################################################################################################################\n#<<<^<^^>v<<^<^><><v<^<<.v^><<<v>><^v<vv>v<vv>^>>^^^^.>>^vv<^<<>^.vv.<>>v>v^>^<><.vv^v.^<^v>^vv<^vv<<>v<<>><<<v^^>v><<<^>#\n#>^^v><.<>>v.v.^>v^..>^<vv>v^><v^<<>v>>.^v^vv^^>.^><^>>^>vv>>v^>vvvv^v^vv<<^<^>^>^v<v^>><><v^^^<v.^v<>^.>v><vv.^<^^^^<>>>#\n#<<.><>v<>.v^<^^v<^^>.^.>>v.^^^v>^^^.<><.^.<>>^<>.v><^^^>^>^^^^><vv^.v^<<^v>v^^^<<<^<..^vv><.^v<vv^>>vv>v.><v^>>^<v^>>vv>#\n#.><^v.>^v>>vv><^>.>>>>>^.>>.>^v>v^>v^^>v^^..>^.^<^.v<>v<<<^^^.vvv>>.<>^vvv>>.^<^v^>^<^<vv>^>^.<>^<<>v.<<<.^<>^>.<v.vvv^>#\n#>><v<>.v><<^^vvv<<<^>^v^v><^>^<>>v.<.<>^<>vv>vv>^.v><<<^<v^vvv.<<<><^<^<^.^^vv>^v^^v<>>vv^<^.^v^v<v^>^.>.v>v^^<v<^<>^>v>#\n#>>><v<.>>v^<.^vvvv<v<^<<v>>v>vv.^><v^.>>vv><<.<^.^v>^^><>^<^v<vvvvv<<<<^^>..>>>v^^>.vv<>.^^^.<v>v^>>^>^^>v^v^^<.>>^^<.<>#\n#<<v>>v<>^><^v<>^>vvv..v.^.<^.v.vv^.<.v<>v>^<^v^<^>><.<>v^v>^<<^<v^^<v^<v^.<v><<<>^^<><>.v><^^^<<^<v>><vv<^<^.^<<^.^<<v>>#\n#>>>^^>v^>^>>v><<>^<v><>>^>.<v<<>v<>^<>^v.<<^.^^^^>^.v>v><v^>vv^^^.>v^<v^^v<<^.vv^>vvv>^>>v<.^^vv<<v.v..^v.^vvv.<.>^<^v>>#\n#><^vv^>>v.<^<v^^><v^>^><<<v^>v><v^^>><vv^><.>>^vv>><vv<^v^v^>^>>.vvvvvv<>>>.>^<vv^<vv<<<^^v^>.v><^.<<^<>^^>.^v><^^vv.^>>#\n#>..<<v<<>>.<^v<<.>v^<..^<^^v^>.><.<v>>v><<>^>>v<vvv>>v<v>.vvvv<.<<<^<^v>^<v^>>v.^v^<^v..^..<^<><>^<<>^>^<<^^.>^^<v<>v<><#\n#<v^v>v<^^<v>>vv^v<..^>v>>>.>^^<>>>^..>>>^<<^<^<>^<v>>^>>><^v^v^<.^<<<^v^.^^^<v>>><^^<>>><..vv.^.^<<v^v.>>^v^^><>><.v^>v>#\n#.<^^<v>v.<v<^.^v^<.v^vvv^vv^<..v<.>>>v<<>>vv>>^.>^.v>vv>.v^<<v^>>v.<^^<^<<>^^.^^>^>>^<^<<^^vv^^.<.><^.>^^<>v<.^v<>v><v<.#\n#>^.v^^><^<.>^v>v><>>>..>.v>vvv<<vv^.>v>.>v.<^vv.^>^vvv>.>v^v>>.>^^<<>>.^.>>>^>v<>v>v^^<>^<^.^><.vv^^<<.^v>>.>>>.^>^^^^^<#\n#..^>vv><^^.>>^^.<<<v..<<>^.<.<^^.v<<<^<<>><>v>v<^^^.<><^<<>v^^<^v^>^v>v<<^<<v<><<>.><..<^vv^<.><>^^><<.>>^.^v><>v^<^v>^>#\n#><<>v>.>^<<v>>>>>^>v..<vv<^<v>>^<<^v<v<<<<.v^>v.v<^>>>v.^><^><>.<>.^v><><<v>^^^^.<<.>^>><>.<<.<^<.^>..^>^.^>^v^v.<.^v>><#\n#><><^v.<<<<>>v<<<<^v<<^^<><<.>^^.^.^.<>vv<<<v>^<><^^<^>^>^^.v<^>>vv^v.>^.<<><vv>>.>.<><^<.<<>.v>.^>>>^<<>.<v>^^v^v^<>.<<#\n#<^>^v^>v^v.<^^>.^>^<<>.<.<<vvv>v>v^v^vvv>v^v^v<^^^<^>>v.^v.^>v><.^^^<>^>^^vv.v>>>^v<<>>^<^.v^>>>><v>v><^<.^v^v^^.<<>><^<#\n#>^>v.><v<^<><>><v<^^<^><<<<^v>^<v^>v<<>^>>.><v<v<^^>^<><><^<.<.<><v^^.^>>v^v^.>><vvvv^v^vv><<^<<^>.v>>>vv>v^v^^<v^^>^>><#\n#<<v^^>vv^^v.<<v<v<^><^v.>.<^v^>^^>^<^^v>>v<^^vv<>^v^.^v<>.<^^>^^v><>^^v.><.v^^>.<<.v^<v<^<>^<^><v^>.>^v>v<v<.>v<.<v.>.<<#\n#>^<<v^vv..vv^v><>>>^>v.>v^..^>^vv>^^>v^v<>.><v<<<<^><>v^.v^>.>^vv>vv>.>^<<.v<^^v^<<>^^^>^.<.v>^><.<^<^>>><.<<^^^>^<^<v<>#\n#<^><v<v><<<>v>>vv.>^<v>><>v>^.v^<v><<^<>>^<^^.<v<<vv>^v.v.<vv<^<<^>v>v.<<<v^><^^>.>>v>.<^vv.^v>^^^<><<^<>>><v^v>>>v<^>v>#\n#>>>^.v><<<>^v<.<.^><v>><>.<.<>v^v>>>^<^^vvv.vv<v<^^^>><^v>>>v>v<>v<v<vv^.^>v<>^>>>^<<v<vv^>><.v<^v<v^<.<>v>^>^><.>v<>.><#\n#<^<v>v.vv>vv^v<.>>.v.^..>^v<.v><>^<.^^<v><^.v^<v^vv^^v^<>>^<^><v.vvv^>.^v>><<>><<<>.<v^<^.<vv^.v<><v^v^>^v^>v<<v^>vv>^v>#\n#<><.v.^v>>><^.<.^<<v>^^><<^>>.^^.v>vv^^.v>^<<^^<<<^>>^>^>v..^v><>v<vv<<v^v<>>><v>^v<><.>v>v>v^^.^>>vv^v.vv<>v^<><^^><vv<#\n#<<v<v>^><^v^<^<v>><.v<.>^vv^>^<v^v>v<.vvv<v<^^>^^^^v^<<v^>v<v^vv>.^vv>v^.>>.^<.>>v<..^v.>^<<v<vvv^<..^<<>^^>v<<><^^><>>>#\n########################################################################################################################.#")})
+(defn goal [state]
+  {:x (dec (:max-x state)) :y (dec (:max-y state))})
 
-;(def init-state {:e {:x -1 :y 0} :grid (parse-grid "#.######\n#>>.<^<#\n#.<..<<#\n#>v.><>#\n#<^v^^>#\n######.#")})
-
-(defn move-candidates [loc max-x max-y]
-  (->> [(update loc :x inc) (update loc :x dec) (update loc :y inc) (update loc :y dec)]
+(defn possible-moves [pos max-x max-y]
+  "Calculates valid moves for the expedition"
+  (->> [(update pos :x inc) (update pos :x dec) (update pos :y inc) (update pos :y dec)]
        (filter #(and (>= (:x %) 0) (>= (:y %) 0) (< (:x %) max-x) (< (:y %) max-y)))))
 
-(defn valid? [loc grid]
-  (not-any? #(and (= (:x loc) (:x %)) (= (:y loc) (:y %))) grid))
+(defn valid? [pos grid]
+  (not-any? #(and (= (:x pos) (:x %)) (= (:y pos) (:y %))) grid))
 
-(defn next-states [{:keys [e grid] :as state}]
-  (let [after-blizzard (into (vector) (map #(move % 25 120) grid))
-        pos (filter #(valid? % after-blizzard) (cons e (move-candidates e 25 120)))]
+(defn next-states [{:keys [e grid max-x max-y] :as state}]
+  "Returns a set of valid states representing possible moves from a single one"
+  (let [after-blizzard (into (vector) (map #(move % max-x max-y) grid))
+        pos (filter #(valid? % after-blizzard) (cons e (possible-moves e max-x max-y)))]
     (->> pos
-         (map (fn [p] {:e p :grid after-blizzard}))
+         (map (fn [p] {:e p :grid after-blizzard :max-x max-x :max-y max-y}))
          set)))
 
-(defn keep-best-states [states]
+(defn manhattan-distance [x y]
+  (+ (Math/abs (- (:x x) (:x y))) (Math/abs (- (:y x) (:y y)))))
+(defn keep-best-states [goal states]
   (->> states
-       (sort-by #(+ (- 25 (:x (:e %))) (- 120 (:y (:e %)))))
-       (take 50)))
+       (sort-by #(manhattan-distance goal (:e %)))
+       (take 100)))
 
-(defn next-minute [states]
+(defn next-minute [states goal]
   (->> states
        (map next-states)
-       (reduce set/union)))
+       (reduce set/union)
+       (keep-best-states goal)))
 
-(defn part1 []
-  (loop [state (next-minute #{init-state})
+(defn part1 [init-state goal]
+  (loop [states (next-minute #{init-state} goal)
          min 1]
-    ;(println min)
-    (if (contains? (set (map :e state)) {:x 24 :y 119})
-      (inc min)
-      (recur (next-minute state) (inc min)))))
+    (if (contains? (set (map :e states)) goal)
+      {:min (inc min) :grid (:grid (first (filter #(= goal (:e %)) states)))}
+      (recur (next-minute states goal) (inc min))))
+  )
 
-(defn part2 [grid start goal]
-  (let [init-state {:e start :grid grid}]
-    (loop [states (next-minute #{init-state})
-           min 1]
-      (println min)
-      (if (contains? (set (map :e states)) goal)
-        {:mins (inc min) :grid (:grid (first (filter #(= goal (:e %)) states)))}
-        (recur (next-minute states) (inc min))))))
+(defn solve1 [init-state]
+  (:min (part1 init-state (goal init-state))))
 
-(defn solve2 []
-  (let [res1 (part2 (:grid init-state) {:x -1 :y 0} {:x 24 :y 119})
-        res2 (part2 (:grid res1) {:x 25 :y 119} {:x 0 :y 0})
-        res3 (part2 (:grid res2) {:x -1 :y 0} {:x 24 :y 119})]
-    (println (:mins res1))
-    (println (:mins res2))
-    (println (:mins res3))
-    (+ (:mins res1) (dec (:mins res2)) (dec (:mins res3)))))
+(defn solve2 [init-state]
+  (let [start start-pos
+        end {:x (dec (:max-x init-state)) :y (dec (:max-y init-state))}
+        first-trip (part1 init-state end)
+        back (part1 (assoc (assoc init-state :grid (:grid first-trip)) :e {:x (inc (:x end)) :y (:y end)}) {:x (inc (:x start)) :y (:y start)})
+        to-goal-again (part1 (assoc init-state :grid (:grid back)) end)
+        ]
+    (+ (:min first-trip) (dec (:min back)) (dec (:min to-goal-again)))))
+
+(defn -main [& _]
+  (let [blizzards-map (parse-blizzards (->> "day24.txt"
+                                            io/resource
+                                            slurp))
+        init-state (merge {:e start-pos} blizzards-map)]
+    (doseq [solve [solve1 solve2]]
+      (println (time (solve init-state))))))
--- 2022/clojure/day24/test/day24/core_test.clj
+++ 2022/clojure/day24/test/day24/core_test.clj
@@ -2,6 +2,27 @@
   (:require [clojure.test :refer :all]
             [day24.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parsing-a-single-line
+  (testing "Parsing a single line of the map."
+    (is (= '({:d \>
+              :x 0
+              :y 0}
+             {:d \>
+              :x 0
+              :y 1}
+             {:d \<
+              :x 0
+              :y 3}
+             {:d \^
+              :x 0
+              :y 4}
+             {:d \<
+              :x 0
+              :y 5}) (parse-line [0 "#>>.<^<#"])))))
+
+(deftest parts-1-and-2-solutions
+  (testing "Both part solutions."
+    (let [input "#.######\n#>>.<^<#\n#.<..<<#\n#>v.><>#\n#<^v^^>#\n######.#"
+          init-state (merge {:e start-pos} (parse-blizzards input))]
+      (is (= 18 (solve1 init-state)))
+      (is (= 54 (solve2 init-state))))))
