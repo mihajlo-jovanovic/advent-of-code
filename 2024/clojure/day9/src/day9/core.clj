270c943af61464f2c72c6f94d7a2f9bc1ffc8013
Mihajlo Jovanovic <linus_18@outlook.com>
2024-12-10 20:09:15 -0500

    day 9: minor cleanup

--- 2024/clojure/day9/src/day9/core.clj
+++ 2024/clojure/day9/src/day9/core.clj
@@ -3,25 +3,43 @@
   (:require [clojure.string :as s]))
 
 (defn parse-input [filename]
-  (let [input (concat (s/trim-newline (slurp filename)) "0")]
-    (map (fn [[n1 n2]] [(Integer/parseInt (str n1)) (Integer/parseInt (str n2))]) (partition 2 input))))
+  (let [input (str (s/trim-newline (slurp filename)) "0")]
+    (map (fn [[x y]]
+           [(Integer/parseInt (str x))
+            (Integer/parseInt (str y))])
+         (partition 2 input))))
 
-(defn helper [diskmap acc]
+(defn defrag
+  "Helper function to compact the diskmap"
+  [diskmap acc]
   (let [[i [n1 n2]] (first diskmap)
-        [j [m1 _]] (last diskmap)]
-    (if (or (empty? diskmap) (= 1 (count diskmap)))
-      (into [] (concat acc (vec (repeat n1 i))))
-      (if (pos-int? n1)
-        (recur (assoc diskmap 0 [i [0 n2]]) (into [] (concat acc (vec (repeat n1 i)))))
-        (if (> n2 m1)
-          (recur (assoc (pop diskmap) 0 [i [n1 (- n2 m1)]]) (into [] (concat acc (vec (repeat m1 j)))))
-          (let [diskmap' (into [] (rest diskmap))]
-            (recur (update-in diskmap' [(dec (count diskmap')) 1 0] - n2) (into [] (concat acc (vec (repeat n2 j)))))))))))
+        [j [m1 _]]  (last diskmap)
+        size (count diskmap)]
+    (cond
+      (<= size 1)
+      (into acc (repeat n1 i))
+
+      (pos-int? n1)
+      (recur (assoc diskmap 0 [i [0 n2]])
+             (into acc (repeat n1 i)))
+
+      (> n2 m1)
+      (recur (assoc (pop diskmap) 0 [i [n1 (- n2 m1)]])
+             (into acc (repeat m1 j)))
+
+      :else
+      (let [diskmap' (vec (rest diskmap))
+            last-idx (dec (count diskmap'))]
+        (recur (update-in diskmap' [last-idx 1 0] - n2)
+               (into acc (repeat n2 j)))))))
+
+(defn score [diskmap]
+  (reduce + (map-indexed (fn [i n] (* i n)) diskmap)))
 
 (defn part1 [input]
   (let [diskmap (into [] (map-indexed vector (parse-input input)))
-        after-compaction (helper diskmap [])]
-    (reduce + (map (fn [[i n]] (* i n)) (map-indexed vector after-compaction)))))
+        after-compaction (defrag diskmap [])]
+    (score after-compaction)))
 
 (defn list-insert [lst elem index]
   (let [[l r] (split-at index lst)]
@@ -33,8 +51,7 @@
   (into (subvec coll 0 pos) (subvec coll (inc pos))))
 
 (defn move-whole-file [diskmap file-id idx-to-insert]
-  (let [file-pos (first (filter (fn [[_ [k [_ _]]]] (= k file-id)) (map-indexed vector diskmap)))
-        [idx [fid [n1 n2]]] file-pos
+  (let [[idx [fid [n1 n2]]] (some (fn [[i [k [x y]]]] (when (= k file-id) [i [k [x y]]])) (map-indexed vector diskmap))
         [_ [_ m2]] (nth diskmap idx-to-insert)
         diskmap' (assoc-in diskmap [idx-to-insert 1 1] 0)
         diskmap'' (vec-remove idx diskmap')
@@ -49,7 +66,7 @@
     (if (zero? file-id)
       diskmap
       (let [[_ [n1 _]] (first (filter (fn [[k [_ _]]] (= k file-id)) diskmap))
-            idx-to-insert (first (first (filter (fn [[_ [_ [_ m2]]]] (>= m2 n1)) (map-indexed vector (take-while (fn [[i [_ _]]] (not= i file-id)) diskmap)))))]
+            idx-to-insert (ffirst (filter (fn [[_ [_ [_ m2]]]] (>= m2 n1)) (map-indexed vector (take-while (fn [[i [_ _]]] (not= i file-id)) diskmap))))]
         (if idx-to-insert
           (recur (move-whole-file diskmap file-id idx-to-insert) (dec file-id))
           (recur diskmap (dec file-id)))))))
@@ -57,10 +74,9 @@
 (defn part2 [input]
   (let [diskmap (into [] (map-indexed vector (parse-input input)))
         after-compaction (helper-p2 diskmap)
-        after-compaction' (into [] (reduce (fn [a [i [n1 n2]]] (concat a (vec (repeat n1 i)) (vec (repeat n2 0)))) [] after-compaction))]
-    (reduce + (map (fn [[i n]] (* i n)) (map-indexed vector after-compaction')))))
+        expanded-form (reduce (fn [a [i [n1 n2]]] (concat a (vec (repeat n1 i)) (vec (repeat n2 0)))) [] after-compaction)]
+    (score expanded-form)))
 
-(defn -main
-  []
+(defn -main []
   (println "Part 1: " (part1 "resources/input.txt"))
-  (println "Part 2: " (part2 "resources/input.txt")))
+  (time (println "Part 2: " (part2 "resources/input.txt"))))
